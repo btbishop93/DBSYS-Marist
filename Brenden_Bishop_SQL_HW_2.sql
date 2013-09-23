aafe9dd9782f8d6f243a632987a113d48349670f
btbishop93 <Brenden@148-100-136-8.foxnet.marist.edu>
2013-09-22 22:39:47 -0400

    SQL HW #2

---
+++ Brenden_Bishop_SQL_HW_2.sql
@@ -0,0 +1,45 @@
+﻿select distinct city 
+from agents
+where aid in (select aid from orders
+		where cid = 'c002');
+
+select distinct pid
+from orders
+where aid in (select aid from orders 
+		where cid in (select cid from customers
+				where city = 'Kyoto')
+		);
+
+select distinct cid, name
+from customers
+where cid not in (select cid from orders
+		where aid in (select aid from orders
+					where aid = 'a03')
+		);
+
+select distinct cid, name
+from customers
+where cid in (select cid from orders
+		where pid = 'p01's
+		intersect
+		select cid from orders
+			where pid = 'p07');
+
+select distinct pid
+from orders
+where cid in (select cid from orders
+		where aid in (select aid from orders
+			where aid = 'a03')
+		);
+
+select distinct name, discount
+from customers
+where cid in (select cid from orders
+		where aid in (select aid from agents
+				where city = 'Dallas' or city = 'Duluth')
+		);
+
+select distinct cid, name 
+from customers
+where discount in (select discount from customers
+		where city = 'Dallas' or city = 'Kyoto');
---
+++ Lab2Example.sql
@@ -0,0 +1,97 @@
+﻿
+CREATE TABLE students (
+  sid      char(4) not null,
+  name     varchar(16),
+  primary key(sid)
+);
+
+
+CREATE TABLE teachers (
+  tid     char(3) not null,
+  name    varchar(16),
+ primary key(tid)
+);        
+
+
+CREATE TABLE subjects (
+  subid      char(3) not null,
+  name       varchar(16),
+ primary key(subid)
+);
+
+
+CREATE TABLE grades (
+  sid     char(4) not null references students(sid),
+  tid     char(3) not null references teachers(tid),
+  subid   char(3) not null references subjects(subid),
+  grade   text
+);
+
+INSERT INTO students (sid, name)
+	values ('s001', 'Simon');
+
+INSERT INTO students (sid, name)
+	values ('s002', 'Alvin');
+
+INSERT INTO students (sid, name)
+	values ('s003', 'Theo');
+
+INSERT INTO students (sid, name)
+	values ('s004', 'Brittany');
+
+INSERT INTO students (sid, name)
+	values ('s005', 'Janette');
+
+INSERT INTO students (sid, name)
+	values ('s006', 'Elena');
+
+INSERT INTO students (sid, name)
+	values ('s007', 'Stu');
+
+-- Teachers --
+INSERT INTO teachers (tid, name)
+	values ('t01', 'Washington');
+
+INSERT INTO teachers (tid, name)
+	values ('t02', 'Adams');
+
+INSERT INTO teachers (tid, name)
+	values ('t03', 'Jefferson');
+
+INSERT INTO teachers (tid, name)
+	values ('t04', 'Lincoln');
+
+--Subjects--
+
+INSERT INTO subjects (subid, name)
+	values ('s01', 'History');
+
+INSERT INTO subjects (subid, name)
+	values ('s02', 'Biology');
+
+INSERT INTO subjects (subid, name)
+	values ('s03', 'SF');
+
+--Grades--
+
+INSERT INTO grades (sid, tid, subid, grade)
+	values ('s001', 't02', 's01', 'A');
+
+INSERT INTO grades (sid, tid, subid, grade)
+	values ('s001', 't02', 's02', 'B');
+
+INSERT INTO grades (sid, tid, subid, grade)
+	values ('s007', 't04', 's03', 'C++');
+
+INSERT INTO grades (sid, tid, subid, grade)
+	values ('s007', 't03', 's02', 'F');
+
+INSERT INTO grades (sid, tid, subid, grade)
+	values ('s006', 't02', 's01', 'B+');
+
+INSERT INTO grades (sid, tid, subid, grade)
+	values ('s002', 't04', 's03', 'C');
+
+INSERT INTO grades (sid, tid, subid, grade)
+	values ('s003', 't04', 's03', 'B');
+
