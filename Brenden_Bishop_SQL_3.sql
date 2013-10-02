8a5daa855c630c8f15466966103d1d020ce72280
btbishop93 <Brenden@148-100-136-8.foxnet.marist.edu>
2013-10-02 09:03:38 -0400

    First 6 questions and start of 7

---
+++ Brenden_Bishop_SQL_3.sql
@@ -0,0 +1,62 @@
+﻿---1---
+select distinct city 
+from agents
+where aid in (select aid from orders
+		where cid = 'c002');
+		
+---2---
+select distinct city
+from agents
+inner join orders
+on agents.aid = orders.aid 
+and orders.cid = 'c002';
+
+---3---
+select distinct pid
+from orders where aid in (select aid from orders
+			where cid in (select cid from customers
+					where city = 'Kyoto'));
+---4---
+select distinct o2.pid
+from orders o1
+	join orders o2
+	on o2.aid = o1.aid 
+	inner join customers
+	on customers.cid = o1.cid 
+	and customers.city = 'Kyoto'
+order by o2.pid asc;
+
+---5---
+select distinct name
+from customers 
+where cid not in (select cid from orders);
+
+---6---
+
+select distinct name
+from customers
+left outer join orders
+on customers.cid = orders.cid 
+where orders.cid is null;
+
+---7---
+
+select distinct customers.name, agents.name
+from customers, agents, orders
+where orders.aid in (select aid from orders 
+		where orders.cid in (select cid from customers
+					where customers.city = agents.city));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Lab3_Example.sql
@@ -0,0 +1,103 @@
+﻿
+drop table if exists Departments;
+
+create table Departments(
+	Did		varchar(4) not null,
+	name		varchar(20) not null,
+	dateFound	date not null,
+	primary key(Did)
+      );
+
+drop table if exists People;
+
+create table People(
+	pid 		varchar(4) not null,
+	firstName 	varchar(20),
+	lastName	varchar(20) not null,
+	hireDate	date not null,
+	Did		varchar(4) not null references Departments(Did),
+	primary key(pid)
+      );
+
+drop table if exists Fy;
+
+create table Fy (
+	year 		int not null,
+	startDate	date not null,
+	endDate		date not null,
+	primary key(year)
+      );
+
+
+----Departments Data----
+
+insert into Departments( Did, name, dateFound)
+		  values('A', 'CS', '6/30/2010');
+
+insert into Departments( Did, name, dateFound)
+		  values('B', 'IT', '1/1/2013');
+
+insert into Departments( Did, name, dateFound)
+		  values('C', 'IS', '7/4/2014');
+
+---People Data---
+
+insert into People( pid, firstName, lastName, hireDate, Did)
+	      values('1', 'Thomas', 'Moran', '1/23/2008', 'A');
+
+insert into People( pid, firstName, lastName, hireDate, Did)
+	      values('2', 'Jimmy', 'Foote', '9/28/2006', 'A');
+
+insert into People( pid, firstName, lastName, hireDate, Did)
+	      values('3', 'Taylor', 'Allen', '4/4/2012', 'B');
+
+insert into People( pid, firstName, lastName, hireDate, Did)
+	      values('4', 'Adam', 'Jones', '11/11/2013', 'B');
+
+insert into People( pid, firstName, lastName, hireDate, Did)
+	      values('5', 'Katie', 'Smith', '11/11/2014', 'B');
+
+----FiscalYear Data-----
+
+insert into fy( year, startDate, endDate )
+	values('2012','7/1/11', '6/30/2012');
+		  	
+
+insert into fy( year, startDate, endDate )
+	values('2013','7/1/12', '6/30/2013');
+
+
+insert into fy( year, startDate, endDate )
+	values('2014','7/1/13', '6/30/2014');
+
+
+insert into fy( year, startDate, endDate )
+	values('2015','7/1/14', '6/30/2015');
+
+
+---QUERIES---
+
+--1. List all people, the fiscal year in which they were hired, and their dept.---
+--2. List all the depts and the number of people in each.---
+--3. List all the fiscal years and the number of people hired in that time span---
+--4. List all the fiscal years and the number of depts founded in that time span---
+--5. List all the depts with no people.---
+--6. List all the people with no dept---
+--7. List all people hired before the dept was founded.
+
+select distinct firstName, lastName, hireDate, departments.name, year
+from people, fy, departments
+	where (hireDate > fy.startDate) and (hireDate < fy.endDate) 
+		and people.Did = departments.Did
+	order by fy.year;
+
+select distinct departments.name, count(people.Did)
+from departments, people
+where people.Did = departments.Did;
+
+
+
+
+
+
+ 
